2ea8b6792679a83553faac890a8840025f41bf83
Daniel <cazaresdaniel0@gmail.com>
2024-07-09 16:48:59 -0700

    6th commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from '..node_modules/react'
+import React from '../node_modules/@types/react/index'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
